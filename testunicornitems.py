67be0af34ff57859d8b597770be7270dcd36de6a
Yağmur Dalkıran <yagmurdalkiran1997@gmail.com>
2024-03-14 09:24:57 +0300

    setUp and tearDown func added

--- testunicornitems.py
+++ testunicornitems.py
@@ -1,9 +1,25 @@
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import  WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+driver = webdriver.Chrome()
+driver.maximize_window()
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self) -> None:
+        self.driver = webdriver.Chrome()
+
+
+    def testlogIn(self):
+        driver = self.driver
+        driver.get("http://unicornitems.com/my-account/")
+
+    def tearDown(self) -> None:
+        self.driver.close()
 
 
 if __name__ == '__main__':
