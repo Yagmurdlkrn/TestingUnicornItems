08950abb9559f50fae3202930b8bdd399f2ef5d9
Yağmur Dalkıran <yagmurdalkiran1997@gmail.com>
2024-03-14 09:32:33 +0300

    test func done

--- testunicornitems.py
+++ testunicornitems.py
@@ -17,6 +17,13 @@ class MyTestCase(unittest.TestCase):
     def testlogIn(self):
         driver = self.driver
         driver.get("http://unicornitems.com/my-account/")
+        element_username = driver.find_element(By.NAME,"username")
+        element_username.send_keys("1")
+        element_password = driver.find_element(By.NAME,"password")
+        element_password.send_keys("1")
+        button_login = driver.find_element(By.NAME,"login")
+        button_login.click()
+        assert "ERROR: INCORRECT USERNAME OR PASSWORD." in driver.page_source
 
     def tearDown(self) -> None:
         self.driver.close()
